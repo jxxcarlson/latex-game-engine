5b2de6eaa031f28cd31e8dc538f898e5c740f6bf
James Carlson <jxxcarlson@gmail.com>
2021-02-07 00:53:15 -0500

    Refactor: add module Style, move code

--- src/Main.elm
+++ src/Main.elm
@@ -8,7 +8,6 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html)
-import Html.Attributes as HA
 import Html.Keyed as Keyed
 import Element exposing (..)
 import Element.Background as Background
@@ -22,6 +21,7 @@ import DocParser exposing(Problem, DocumentDescription)
 import MiniLatex.EditSimple
 import Config
 import Random
+import Style exposing(..)
 import Utility
 import Tree.Zipper as Zipper exposing(Zipper)
 import Markdown.Option exposing (MarkdownOption(..), OutputOption(..))
@@ -219,7 +219,7 @@ fileStatus mstr =
 -- VIEW
 --
 
-gray g = rgb255 g g g
+
 
 view : Model -> Html Msg
 view model =
@@ -326,30 +326,6 @@ renderedSource counter sourceText =
         ) |> Element.html
 
 
-renderedSourceStyle : List (Html.Attribute msg)
-renderedSourceStyle =
-    textStyle "300px" "400px" "#fff"
-
-
-textStyle : String -> String -> String -> List (Html.Attribute msg)
-textStyle width height color =
-    [ HA.style "width" width
-    , HA.style "height" height
-    , HA.style "margin-top" "18px"
-    , HA.style "margin-bottom" "18px"
-    , HA.style "background-color" color
-    , HA.style "margin-right" "20px"
-    , HA.style "white-space" "pre-wrap"
-    , HA.style "padding" "20px"
-    , HA.style "overflow" "scroll"
-    , HA.style "float" "left"
-    , HA.style "border-width" "1px"
-    , HA.style "font-size" "14px"
-    , HA.style "line-height" "1.5"
-    ]
-fixLines : String -> String
-fixLines str =
-   String.replace "\n" " " str
 
 showIf : Bool -> Element Msg -> Element Msg
 showIf show element =
@@ -375,24 +351,7 @@ showComment seed mprob =
                      , column [Font.size 13, width (px 380), padding 10] [renderMath seed commentStyle prob.comment]
                      ]
 
-hintStyle = [  HA.style "background-color" "white"
-             , HA.style "width" "540px"
-             , HA.style "margin-left" "-8px"
-             , HA.style "padding-top" "1px"
-             , HA.style "padding-bottom" "1px"
-             , HA.style "padding-left" "9px"
-             , HA.style "padding-right" "8px"
-             ]
 
-commentStyle = [  HA.style "background-color" "white"
-             , HA.style "width" "320px"
-             , HA.style "margin-top" "0px"
-             , HA.style "margin-left" "-8px"
-             , HA.style "padding-top" "2px"
-             , HA.style "padding-bottom" "2px"
-             , HA.style "padding-left" "9px"
-             , HA.style "padding-right" "8px"
-             ]
 
 viewEditor : Model -> Element Msg
 viewEditor model =
@@ -487,7 +446,6 @@ toggleInfo showInfo =
             }
         ]
 
-labelStyle = [ centerX, centerY, Font.size 14]
 
 loadButton : Element Msg
 loadButton =
@@ -525,34 +483,3 @@ nextButton =
             }
         ]
 
---
--- STYLE
---
-
-
-mainColumnStyle =
-    let
-      g = 200
-    in
-    [ centerX
-    , centerY
-    , Background.color (rgb255 g g g)
-    , paddingXY 20 20
-    , width (px 600)
-    , height (px 800)
-    ]
-
-rhsColumnStyle =
-    [ centerX
-    , centerY
-    , Background.color (gray 200)
-    , paddingXY 20 20
-    , width (px 400)
-    , height (px 800)
-    ]
-
-buttonStyle =
-    [ Background.color (rgb255 40 40 40)
-    , Font.color (rgb255 255 255 255)
-    , paddingXY 15 8
-    ]
---
+++ src/Style.elm
@@ -0,0 +1,83 @@
+module Style exposing (..)
+
+
+import Element exposing (..)
+import Element.Background as Background
+import Element.Font as Font
+import Html.Attributes as HA
+import Html exposing (Html)
+
+-- STYLE
+
+gray g = rgb255 g g g
+
+labelStyle = [ centerX, centerY, Font.size 14]
+
+mainColumnStyle =
+    let
+      g = 200
+    in
+    [ centerX
+    , centerY
+    , Background.color (rgb255 g g g)
+    , paddingXY 20 20
+    , width (px 600)
+    , height (px 800)
+    ]
+
+rhsColumnStyle =
+    [ centerX
+    , centerY
+    , Background.color (gray 200)
+    , paddingXY 20 20
+    , width (px 400)
+    , height (px 800)
+    ]
+
+buttonStyle =
+    [ Background.color (rgb255 40 40 40)
+    , Font.color (rgb255 255 255 255)
+    , paddingXY 15 8
+    ]
+
+hintStyle = [  HA.style "background-color" "white"
+             , HA.style "width" "540px"
+             , HA.style "margin-left" "-8px"
+             , HA.style "padding-top" "1px"
+             , HA.style "padding-bottom" "1px"
+             , HA.style "padding-left" "9px"
+             , HA.style "padding-right" "8px"
+             ]
+
+commentStyle = [  HA.style "background-color" "white"
+             , HA.style "width" "320px"
+             , HA.style "margin-top" "0px"
+             , HA.style "margin-left" "-8px"
+             , HA.style "padding-top" "2px"
+             , HA.style "padding-bottom" "2px"
+             , HA.style "padding-left" "9px"
+             , HA.style "padding-right" "8px"
+             ]
+
+
+renderedSourceStyle : List (Html.Attribute msg)
+renderedSourceStyle =
+    textStyle "300px" "400px" "#fff"
+
+
+textStyle : String -> String -> String -> List (Html.Attribute msg)
+textStyle width height color =
+    [ HA.style "width" width
+    , HA.style "height" height
+    , HA.style "margin-top" "18px"
+    , HA.style "margin-bottom" "18px"
+    , HA.style "background-color" color
+    , HA.style "margin-right" "20px"
+    , HA.style "white-space" "pre-wrap"
+    , HA.style "padding" "20px"
+    , HA.style "overflow" "scroll"
+    , HA.style "float" "left"
+    , HA.style "border-width" "1px"
+    , HA.style "font-size" "14px"
+    , HA.style "line-height" "1.5"
+    ]
